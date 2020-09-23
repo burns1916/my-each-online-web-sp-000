c1fdad99507a36aa5399cf2843db96d2138f0ed3
Ryan K Burns <ryankburns@gmail.com>
2020-09-23 15:33:33 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,8 @@
 def my_each(array) # put argument(s) here
   # code here
   i = 0
-  while i < array.length do |word|
-    puts word
+  while i < array.length
+    puts array[i]
     i += 1
     yield
   end
